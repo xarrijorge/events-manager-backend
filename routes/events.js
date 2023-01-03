090bfd594a2368925ec3e7bf154181dd7cf89103
Xarri Jorge <kodinglife@gmail.com>
2023-01-03 00:46:55 +0000

    Add needed packages

--- routes/events.js
+++ routes/events.js
@@ -1,7 +1,7 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express');
+let router = express.Router();
 
-/* GET home page. */
+/* GET all events. */
 router.get('/', function (req, res, next) {
   res.send('Welcome, Young Folks');
 });
